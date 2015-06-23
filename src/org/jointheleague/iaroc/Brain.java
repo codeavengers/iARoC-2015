20417ed8b681e84c06175c39adc4d31a20f5c820
codeavengers <codeavengers@WTS1.local>
2015-06-23 13:47:54 -0700

    added the "drag race" code to main code.

--- src/org/jointheleague/iaroc/Brain.java
+++ src/org/jointheleague/iaroc/Brain.java
@@ -1,5 +1,7 @@
 package org.jointheleague.iaroc;
 
+import android.os.SystemClock;
+
 import ioio.lib.api.IOIO;
 import ioio.lib.api.exception.ConnectionLostException;
 import org.wintrisstech.irobot.ioio.IRobotCreateAdapter;
@@ -28,59 +30,22 @@ public class Brain extends IRobotCreateAdapter {
 
     /* This method is called repeatedly. */
     public void loop() throws ConnectionLostException {
+
         readSensors(SENSORS_INFRARED_BYTE);
-        readSensors(SENSORS_WALL);
         readSensors(SENSORS_BUMPS_AND_WHEEL_DROPS);
-        int sensor = getInfraredByte();
-
-
-        if(sensor == 255)
+        int ir = getInfraredByte();
+        //iARoc2015GoldRush();
+        //iARoc2015DragRace();
+        driveDirect(500,500);
+        if(isBumpLeft());
         {
-            driveDirect(550,550);
-            dashboard.speak("nothing");
+        driveDirect(-300,-500);
+            driveDirect(500,500);
         }
-
-        if(sensor == 250)
+        if(isBumpRight());
         {
-            driveDirect(500,450);
-        }
-
-        if (sensor == 244)
-        {
-            driveDirect (500, 410);
-            dashboard.speak("green buoy");
-        }
-        if (sensor == 242)
-        {
-            driveDirect(450, 450);
-            dashboard.speak("force field");
-        }
-
-            if(sensor == 246)
-            {
-                driveDirect(450, 500);
-            }
-
-            if(sensor == 254)
-            {
-                driveDirect(500,500);
-            }
-
-
-            if (sensor == 248)
-            {
-
-                driveDirect(430, 500);
-                dashboard.speak ("red buoy");
-                                                           }
-
-            if(sensor == 252)
-            {
-                driveDirect(550,550);
-            }
-
-
-
+            driveDirect(-500,-300);
+            driveDirect(500,500);
         }
 
 
@@ -90,25 +55,52 @@ public class Brain extends IRobotCreateAdapter {
 
 
 
+    }
 
-            //dashboard.speak("" + getInfraredByte());
-            //readSensors(SENSORS_WALL);
-            //dashboard.speak("" + getWallSignal());
-            //if(getWallSignal()>1){
-            //dashboard.speak("nobody puts baby in a corner");
+    public void iARoc2015GoldRush() {
+        //           driveDirect(500, 500);
+//            SystemClock.sleep(1000);
+//          driveDirect(500, -500);
+//      SystemClock.sleep(1000);
 
+//        dashboard.log("." + ir);
+//        if (ir == 248) {
+//            driveDirect(475, 500);
+//
+//        }
+//        if (ir == 244) {
+//            driveDirect(500, 475);
+//        }
+//        if (ir == 255) {
+//            driveDirect(500, 500);
+//
+//        }
+//        if (ir == 246) {
+//            driveDirect(450, 500);
+//        }
+//        if (ir == 242) {
+//            driveDirect(450, 450);
+//        }
+//        if (ir == 254) {
+//            driveDirect(500, 500);
+//        }
+//        if (ir == 250) {
+//            driveDirect(500, 450);
+//        }
+//}}
+    }
 
-
-            // readSensors(SENSORS_DISTANCE);
-            //number = number + getDistance();
-            // readSensors(SENSORS_BUMPS_AND_WHEEL_DROPS);
-            // readSensors(SENSORS_DISTANCE);
-            //number = number + getDistance();
-            //if(isBumpRight() || isBumpLeft()) {
-            //readSensors(SENSORS_DISTANCE);
-            //number = number + getDistance();
-            //dashboard.speak(number + ".");
-
-
-
+    public void iARoc2015DragRace() throws ConnectionLostException {
+        driveDirect(500, 500);
+        if(isBumpRight()){
+            driveDirect(0, -500);
+            SystemClock.sleep(500);
+            driveDirect(500, 500);
         }
+        if(isBumpLeft()){
+            driveDirect(-500, 0);
+            SystemClock.sleep(500);
+            driveDirect(500, 500);
+        }
+    }
+}
